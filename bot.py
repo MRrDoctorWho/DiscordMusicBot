dde7b4042c590c24ae65e42ae2666eb25cb185d5
MRrDoctorWho <37578458+MRrDoctorWho@users.noreply.github.com>
2018-03-20 21:32:11 +0600

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,106 @@
+                        expire_in=20)
+
+            if params:
+                docs = getattr(handler, '__doc__', None)
+                if not docs:
+                    docs = 'Usage: {}{} {}'.format(
+                        self.config.command_prefix,
+                        command,
+                        ' '.join(args_expected)
+                    )
+
+                docs = '\n'.join(l.strip() for l in docs.split('\n'))
+                await self.safe_send_message(
+                    message.channel,
+                    '```\n%s\n```' % docs.format(command_prefix=self.config.command_prefix),
+                    expire_in=60
+                )
+                return
+
+            response = await handler(**handler_kwargs)
+            if response and isinstance(response, Response):
+                content = response.content
+                if response.reply:
+                    content = '%s, %s' % (message.author.mention, content)
+
+                sentmsg = await self.safe_send_message(
+                    message.channel, content,
+                    expire_in=response.delete_after if self.config.delete_messages else 0,
+                    also_delete=message if self.config.delete_invoking else None
+                )
+
+        except (exceptions.CommandError, exceptions.HelpfulError, exceptions.ExtractionError) as e:
+            print("{0.__class__}: {0.message}".format(e))
+
+            expirein = e.expire_in if self.config.delete_messages else None
+            alsodelete = message if self.config.delete_invoking else None
+
+            await self.safe_send_message(
+                message.channel,
+                '```\n%s\n```' % e.message,
+                expire_in=expirein,
+                also_delete=alsodelete
+            )
+
+        except exceptions.Signal:
+            raise
+
+        except Exception:
+            traceback.print_exc()
+            if self.config.debug_mode:
+                await self.safe_send_message(message.channel, '```\n%s\n```' % traceback.format_exc())
+
+    async def on_voice_state_update(self, before, after):
+        if not all([before, after]):
+            return
+
+        if before.voice_channel == after.voice_channel:
+            return
+
+        if before.server.id not in self.players:
+            return
+
+        my_voice_channel = after.server.me.voice_channel  # This should always work, right?
+
+        if not my_voice_channel:
+            return
+
+        if before.voice_channel == my_voice_channel:
+            joining = False
+        elif after.voice_channel == my_voice_channel:
+            joining = True
+        else:
+            return  # Not my channel
+
+        moving = before == before.server.me
+
+        auto_paused = self.server_specific_data[after.server]['auto_paused']
+        player = await self.get_player(my_voice_channel)
+
+        if after == after.server.me and after.voice_channel:
+            player.voice_client.channel = after.voice_channel
+
+        if not self.config.auto_pause:
+            return
+
+        if sum(1 for m in my_voice_channel.voice_members if m != after.server.me):
+            if auto_paused and player.is_paused:
+                print("[config:autopause] Unpausing")
+                self.server_specific_data[after.server]['auto_paused'] = False
+                player.resume()
+        else:
+            if not auto_paused and player.is_playing:
+                print("[config:autopause] Pausing")
+                self.server_specific_data[after.server]['auto_paused'] = True
+                player.pause()
+
+    async def on_server_update(self, before:discord.Server, after:discord.Server):
+        if before.region != after.region:
+            self.safe_print("[Servers] \"%s\" changed regions: %s -> %s" % (after.name, before.region, after.region))
+
+            await self.reconnect_voice_client(after)
+
+
+if __name__ == '__main__':
+    bot = MusicBot()
+    bot.run()
